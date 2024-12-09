b807dac48bc1a8811f66710cea476aabdbef2e06
nithila <nithila.annadurai@uconn.edu>
2024-12-09 14:56:53 -0500

    days 1-3

--- scripts/day3.py
+++ scripts/day3.py
@@ -1,20 +1,29 @@
-import re 
+import re
 
 with open('../inputs/input3.txt', 'r') as f:
     text = f.readlines()
+    #text = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))undon't()mul(1,1)"]
 
     sum = 0
+    do = True
     for line in text:
-        for i in range(len(line)):
-            inner_sum = 0
-            idx = re.findall(r'mul\(\d{1,3},\d{1,3}\)', line)
-            
-            for j in idx:
-                string = j[4:-1].split(',')
-                prd = int(string[0]) * int(string[1])
-                inner_sum += prd
-        sum += inner_sum
+        muls = []
+        idx = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line)
 
-        
+        for j in idx:
+            if j == 'do()':
+                do = True
+            elif j == "don't()":
+                do = False
+            elif do:
+                muls.append(j)     
+
+        inn_sum = 0
+        for mul in muls:
+            string = mul[4:-1].split(',')
+            prd = int(string[0]) * int(string[1])
+            inn_sum += prd
+
+        sum += inn_sum
+
     print(sum)
-    
